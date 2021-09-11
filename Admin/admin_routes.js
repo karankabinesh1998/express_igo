cff821e3b2f6b59e39e0a6599516546adb9eb47e
karankabinesh1998 <karankabinesh@gmail.com>
2021-09-11 22:27:10 +0530

    date issue changes

--- Admin/admin_routes.js
+++ Admin/admin_routes.js
@@ -16,7 +16,8 @@ var whitelist = [
   "igotaxy.com",
   "192.168.1.105:3008",
   "10.0.2.2:8081",
-  "www.igotaxy.in"
+  "www.igotaxy.in",
+  "www.cp.igotaxy.in"
    ];
 
 const corsOptionsDelegate = (req, res, next) => {
